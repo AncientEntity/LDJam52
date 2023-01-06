9da0d72c9eb692cb7af500d4c2970c342be2e1c6
Ryan <rogoclub@outlook.com>
2023-01-06 12:40:55 -0800

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public int money;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+}
