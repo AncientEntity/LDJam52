ab4bcb5911a668bcabd3c4923babdc1ac4f047b1
Ryan <rogoclub@outlook.com>
2023-01-06 14:55:56 -0800

    planet sprites added

--- Assets/Scripts/Level/Planet.cs
+++ Assets/Scripts/Level/Planet.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Planet : SpaceBody
 {
     public bool isGasGiant = false;
+    public bool randomColor = false;
     public SpriteRenderer sR;
+    public Sprite[] possibleSprites;
 
     private void Start()
     {
-        Color c = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        if(isGasGiant) { c.a = 0.8f; }
+        Color c = new Color(1f,1f,1f);
+        if (randomColor)
+        {
+            c = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+            if(isGasGiant) { c.a = 0.8f; }
         sR.color = c;
+        sR.sprite = possibleSprites[Random.Range(0, possibleSprites.Length)];
     }
 
 }
