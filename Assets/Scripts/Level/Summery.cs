35f90d8fb29ea83c9ebc27e8e67b59556947fde6
eColeye <81527120+eColeye@users.noreply.github.com>
2023-01-08 14:19:15 -0800

    summery

---
+++ Assets/Scripts/Level/Summery.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summery : MonoBehaviour
+{
+    public static int starting_money = 0;
+    public static int spent_money = 0;
+    public static int money_made = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
